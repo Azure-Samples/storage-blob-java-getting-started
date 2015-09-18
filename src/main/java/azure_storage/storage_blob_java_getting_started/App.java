2961e16f8e350cbe2f43d83f0ce3d6580fe10f61
mac1228 <mac1228@gmail.com>
2015-09-18 10:29:57 -0700

    added config file, storage emulator, and random container id. also made style changes

--- src/main/java/azure_storage/storage_blob_java_getting_started/App.java
+++ src/main/java/azure_storage/storage_blob_java_getting_started/App.java
@@ -1,67 +1,84 @@
-//----------------------------------------------------------------------------------
-// Microsoft Developer & Platform Evangelism
-//
-// Copyright (c) Microsoft Corporation. All rights reserved.
-//
-// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
-// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-//----------------------------------------------------------------------------------
-// The example companies, organizations, products, domain names,
-// e-mail addresses, logos, people, places, and events depicted
-// herein are fictitious.  No association with any real company,
-// organization, product, domain name, email address, logo, person,
-// places, or events is intended or should be inferred.
-//----------------------------------------------------------------------------------
+/**
+ * Copyright Microsoft Corporation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package azure_storage.storage_blob_java_getting_started;
 
 import java.io.*;
+import java.util.Properties;
+import java.util.UUID;
 
 import com.microsoft.azure.storage.*;
 import com.microsoft.azure.storage.blob.*;
 
-/// <summary>
-/// Azure Storage Blob Sample - Demonstrate how to use the Blob Storage service. 
-/// Blob storage stores unstructured data such as text, binary data, documents or media files. 
-/// Blobs can be accessed from anywhere in the world via HTTP or HTTPS.
-/// 
-/// Documentation References: 
-/// - What is a Storage Account - http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/
-/// - Getting Started with Blobs - http://azure.microsoft.com/en-us/documentation/articles/storage-java-how-to-use-blob-storage/
-/// - Blob Service Concepts - http://msdn.microsoft.com/en-us/library/dd179376.aspx 
-/// - Blob Service REST API - http://msdn.microsoft.com/en-us/library/dd135733.aspx
-/// - Blob Service Java API - http://azure.github.io/azure-storage-java/
-/// - Delegating Access with Shared Access Signatures - http://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-shared-access-signature-part-1/
-/// - Storage Emulator - http://msdn.microsoft.com/en-us/library/azure/hh403989.aspx
-/// </summary>
+/**
+* Azure Storage Blob Sample - Demonstrate how to use the Blob Storage service. 
+* Blob storage stores unstructured data such as text, binary data, documents or media files. 
+* Blobs can be accessed from anywhere in the world via HTTP or HTTPS.
+* 
+* Documentation References: 
+*  - What is a Storage Account - http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/
+*  - Getting Started with Blobs - http://azure.microsoft.com/en-us/documentation/articles/storage-java-how-to-use-blob-storage/
+*  - Blob Service Concepts - http://msdn.microsoft.com/en-us/library/dd179376.aspx 
+*  - Blob Service REST API - http://msdn.microsoft.com/en-us/library/dd135733.aspx
+*  - Blob Service Java API - http://azure.github.io/azure-storage-java/
+*  - Delegating Access with Shared Access Signatures - http://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-shared-access-signature-part-1/
+*  - Storage Emulator - http://msdn.microsoft.com/en-us/library/azure/hh403989.aspx
+*/
 
 public class App {
-	// *************************************************************************************************************************
-	// Instructions: This sample can be run by updating storageConnectionString with your Storage account name and account key
-	//
-	//      1. Create a Storage Account through the Azure Portal and replace AccountName and AccountKey in 
-	//         storageConnectionString with your account name and account key. See http://go.microsoft.com/fwlink/?LinkId=325277 for more information
-	//      2. Set breakpoints and run the project. 
-	// 
-	// *************************************************************************************************************************
-	public static final String storageConnectionString =
-			"DefaultEndpointsProtocol=https;"
-					+ "AccountName=YourAccountName;"
-					+ "AccountKey=YourAccountKey";
+	/**
+    * Instructions: This sample can be run using either the Azure Storage Emulator (Windows) or by  
+    * updating the config.properties file with your Storage account name and key.
+	*
+    * To run the sample using the Azure Storage Emulator (default option)
+    *      1. Download the Azure Storage Emulator https://azure.microsoft.com/en-us/downloads/ 
+	*	   2. Start the emulator (once only) by pressing the Start button or the Windows key and searching for it
+    *         by typing "Azure Storage Emulator". Select it from the list of applications to start it.
+    *      3. Set breakpoints and run the project. 
+    * 
+    * To run the sample using the Storage Service
+    *      1. Open the config.properties file and comment out the connection string for the emulator (UseDevelopmentStorage=True) and
+    *         uncomment the connection string for the storage service (AccountName=[]...)
+    *      2. Create a Storage Account through the Azure Portal and provide your [AccountName] and [AccountKey] in 
+    *         the config.properties file. See https://azure.microsoft.com/en-us/documentation/articles/storage-create-storage-account/ for more information
+    *      3. Set breakpoints and run the project. 
+    * 
+    */
 
 	public static void main(String[] args) {
 		System.out.println("Azure Storage Blob Sample\n ");
 
-		// Block blob basics
-		System.out.println("Block Blob Sample");
-		BasicStorageBlockBlobOperations();  
+		try{
+			// Retrieve Storage Connection String
+			Properties prop = new Properties();
+			prop.load(App.class.getClassLoader().getResourceAsStream("config.properties"));
+			String storageConnectionString = prop.getProperty("StorageConnectionString");
+			
+			// Block blob basics
+			System.out.println("Block Blob Sample");
+			BasicStorageBlockBlobOperations(storageConnectionString); 
+		}
+		catch (IOException ex){
+			System.out.print("IOException encountered: ");
+			System.out.println(ex.getMessage());
+			System.exit(-1);
+		}
 	}
-
-	/// <summary>
-    /// Basic operations to work with block blobs
-    /// </summary>
-	public static void BasicStorageBlockBlobOperations(){
+	
+	// Basic operations to work with block blobs
+	public static void BasicStorageBlockBlobOperations(String storageConnectionString){
 		try {
 			String imageToUpload = "HelloWorld.png";
 
@@ -74,13 +91,13 @@ public class App {
 
 			// Create a container for organizing blobs within the storage account.
 			System.out.println("1. Creating Container");
-			CloudBlobContainer container = blobClient.getContainerReference("democontainerblockblob");
+			CloudBlobContainer container = blobClient.getContainerReference("democontainerblockblob" + UUID.randomUUID().toString().replace("-", ""));
 			container.createIfNotExists();
 
 			// To view the uploaded blob in a browser, you have two options. The first option is to use a Shared Access Signature (SAS) token to delegate 
 			// access to the resource. See the documentation links at the top for more information on SAS. The second approach is to set permissions 
 			// to allow public access to blobs in this container. Uncomment the three lines of code below to use this approach. Then you can view the image 
-			// using: https://[InsertYourStorageAccountNameHere].blob.core.windows.net/democontainerblockblob/HelloWorld.png
+			// using: https://[InsertYourStorageAccountNameHere].blob.core.windows.net/democontainerblockblob[randomUUID]/HelloWorld.png
 
 			// BlobContainerPermissions containerPermissions = new BlobContainerPermissions();
 			// containerPermissions.setPublicAccess(BlobContainerPublicAccessType.CONTAINER);
@@ -103,17 +120,13 @@ public class App {
 			System.out.println("4. Download Blob from " + blockBlob.getUri());
 			File destinationFile = new File(sourceFile.getParentFile(), "CopyOf" + imageToUpload);
 			blockBlob.downloadToFile(destinationFile.getAbsolutePath());
-
+			
 			// Clean up after the demo 
 			System.out.println("5. Delete block Blob");
 			blockBlob.delete();
-
-			// When you delete a container it could take several seconds before you can recreate a container with the same
-			// name - hence to enable you to run the demo in quick succession, the container is not deleted. If you want 
-			// to delete the container, uncomment the two lines of code below. 
 			
-			// System.out.println("6. Delete Container");
-			// container.delete();
+			System.out.println("6. Delete Container");
+			container.delete();
 		}
 		catch (FileNotFoundException fileNotFoundException) {
 			System.out.print("FileNotFoundException encountered: ");
